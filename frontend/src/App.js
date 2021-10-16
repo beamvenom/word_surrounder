a2e1b19de440ef42a96b51fee1dd8f801cbea706
beamvenom <49659993+beamvenom@users.noreply.github.com>
2021-10-16 13:34:05 +0200

    Frontend first version

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FileUpload from './components/file-upload/file-upload.component'
+
+function App() {
+  return (
+    <div>
+      <form>
+        <FileUpload />
+      </form>
+    </div>
+  )
+}
+export default App
