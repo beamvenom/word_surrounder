bea7063f0c41e572ae18254f68d7882d7d1f64a7
Tony <tony.dang96@gmail.com>
2021-10-17 09:58:04 +0200

    test most frequent word successful

--- backend/tests/test_text_processor.py
+++ backend/tests/test_text_processor.py
@@ -1,6 +1,11 @@
 from src import text_processor
 
 def test_one_word():
-    input = "hej"
+    input = "The"
     output = text_processor.process_text(input)
     assert output == f"foo{input}bar"
+
+def test_most_frequent_word():
+    input = "the the the ok ok"
+    output = text_processor.process_text(input)
+    assert output == "foothebar foothebar foothebar ok ok"
