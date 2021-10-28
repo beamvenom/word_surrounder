5de20b622a083010238b9504e7c7704fb7ce6d68
beamvenom <49659993+beamvenom@users.noreply.github.com>
2021-10-28 21:39:11 +0200

    changed to screen.getByTestId to match conventions

--- frontend/src/components/file_upload/__test__/FileUpload.test.js
+++ frontend/src/components/file_upload/__test__/FileUpload.test.js
@@ -1,40 +1,41 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent , screen} from '@testing-library/react'
 import FileUpload from '../FileUpload.jsx'
 import renderer from 'react-test-renderer'
 import '@testing-library/jest-dom'
 
-let getByTestId
+let setUploadedFile
 beforeEach(() => {
-    const setUploadedFile = jest.fn()
-    const component = render(<FileUpload setUploadedFile = {setUploadedFile}></FileUpload>)
-    getByTestId = component.getByTestId
+    setUploadedFile = jest.fn()
+    render(<FileUpload setUploadedFile = {setUploadedFile}></FileUpload>)
     
   });
 
 test("Upload text renders correctly",() =>{
-    expect(getByTestId("uploadText")).toHaveTextContent("Upload a text and the most occuring word in the text will be surronded by foo and bar!")
+    expect(screen.getByTestId("uploadText")).toHaveTextContent("Upload a text and the most occuring word in the text will be surronded by foo and bar!")
 })
 test("File field text renders correctly",() =>{
-    expect(getByTestId("fileFieldText")).toHaveTextContent("Upload a text file")
+    expect(screen.getByTestId("fileFieldText")).toHaveTextContent("Upload a text file")
 })
 test("File field accepts text file correctly",() =>{
     const file = new File(['hello'], 'text-file.txt', { type: 'text/plain' })
-    fireEvent.change(getByTestId("fileField"), {
+    fireEvent.change(screen.getByTestId("fileField"), {
         target: { files: [file]},
     })
     let text = document.querySelector('input[type=file]')
-    expect(text.files[0].name).toBe('text-file.txt');
-    expect(getByTestId("uploadText")).toHaveTextContent('Success')
+    expect(text.files[0].name).toBe('text-file.txt')
+    expect(screen.getByTestId("uploadText")).toHaveTextContent('...Processing...')
+    expect(setUploadedFile).toHaveBeenCalledTimes(1)
 })
 test("File field denies incorrect file format correctly",() =>{
     const file = new File(['(⌐□_□)'], 'png-file.png', { type: 'image/png' })
-    fireEvent.change(getByTestId("fileField"), {
+    fireEvent.change(screen.getByTestId("fileField"), {
         target: { files: [file]},
     })
     let image = document.querySelector('input[type=file]')
-    expect(image.files[0].name).toBe('png-file.png');
-    expect(getByTestId("uploadText")).toHaveTextContent('The supported file formats are .txt .rtf .file .md')
+    expect(image.files[0].name).toBe('png-file.png')
+    expect(screen.getByTestId("uploadText")).toHaveTextContent('The supported file formats are .txt .rtf .file .md')
+    expect(setUploadedFile).toHaveBeenCalledTimes(0)
 })
 
 test("matches snapshot", () => {
